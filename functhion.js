889bd642e32a53da159c273f59bf4d9d655b764e
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-01-22 14:00:39 +0430

    add code for lab manual

---
+++ functhion.js
@@ -0,0 +1,18 @@
+function squareRoot (num){
+    return num*num
+}
+console.log (squareRoot(9))
+//سوال دوم
+function num(a,b){
+    if(a>b){
+        console.log(a);
+    }else{
+        console.log(b)
+    }
+}
+num(20,16)
+//سوال سوم
+function greating(name,lastName){
+    console.log("سلام"+" "+name+lastName+" "+"خوش امدید")
+}
+greating("نازنین ","حسینی ")
