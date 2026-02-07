e7422bc617cd9221d1464d804692bb0d6ffbebc9
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-02-07 19:45:16 +0430

    chenges

---
+++ ArrayFunction.html/app.js
@@ -0,0 +1,17 @@
+const numbers =[1,2,3,4,5];
+numbers.push(6,7,8,9,10);
+numbers.pop();
+numbers.unshift(0);
+numbers.shift();
+
+const num=[12,34,54,76,88,99];
+const result = num.slice(1,2);
+
+const name = ["Nazanin","Masoda","Raihan","Yasamin"];
+name.splice(1,0,"Mahdia");
+const names = ["Mozghan","Shekiba","Rahima"];
+const AllNames = name.concat(names);
+
+const text = ["Hi", "my", "name", "is", "Nazanin"];
+const newText = text.join(" ");
+
