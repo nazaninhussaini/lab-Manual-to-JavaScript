6ab36e9ca9c67ee09595cd01e12e87d6cf5b2da8
nazaninhussaini <nazaninhussaini@gmail.com>
2026-07-08 21:27:46 +0430

    Added codes for post componend

--- New-project-next-js/new-next-app/src/app/posts.tsx/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function PostsPage(){
-    return (
-        <div className="w-full max-w-5xl flex justify-center items-center bg-stone-100">
-            <div></div>
-        </div>
-    )
-}
--- New-project-next-js/new-next-app/src/app/posts/[id]/page.tsx
+++ New-project-next-js/new-next-app/src/app/posts/[id]/page.tsx
@@ -1,18 +1,18 @@
 import { posts } from "../../../../public/db/post";
 
-async  function page({params} : {params : Promise<{id: number}>}){
+async function pagePost({params} : {params : Promise<{id: number}>}){
     const {id} = await params;
     const foundPost = posts.find((post)=>{
         return post.id == id;
     })
     if (!foundPost ) return ;
     return (
-        <div className="w-full h-screen flex justify-center items-center">
-            <div className="border rounded-2xl w-1/2 p-5">
+        <div className="w-full h-screen gap-2 flex justify-center items-center">
+            <div className="border shadow-2xl rounded-2xl w-1/2 p-5">
             <h1 className="text-3xl font-semibold ">{foundPost.title}</h1>
-            <p className="text-sm text-gray-800">{foundPost.body}</p>
+            <p className="text-sm text-gray-500">{foundPost.body}</p>
             </div>
         </div>
     )
 }
-export default page;
+export default pagePost;
