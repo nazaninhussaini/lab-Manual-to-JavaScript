f9f51642e1d56fc31883cc2f68b200049d3f7b53
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-07-24 15:16:22 +0430

    added codes for pages

--- New-project-next-js/new-next-app/src/app/(root)/about/page.tsx
+++ New-project-next-js/new-next-app/src/app/(root)/about/page.tsx
@@ -5,7 +5,8 @@ function getRandomeNumber(num :number){
 function page() {
    const num = getRandomeNumber(2);
    if(num ==1){
-    throw new Error("Error in about page")
+    throw new Error("Error in about page");
+    
    } 
   return (
     <div className='w-full h-screen flex justify-center items-center text-3xl font-normal text-blue-500'>
--- New-project-next-js/new-next-app/src/app/layout.tsx
+++ New-project-next-js/new-next-app/src/app/layout.tsx
@@ -27,9 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} h-full antialiased`}
-    >
-
+      className={`${geistSans.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         <Navbar/>
         {children}
